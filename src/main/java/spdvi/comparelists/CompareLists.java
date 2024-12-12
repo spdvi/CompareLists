3b4e76995b7d627ef7376559ed8b8ff9c31d0e15
spdvi <gjm@paucasesnovescifp.cat>
2024-12-12 14:33:47 +0100

    Segona versió amb restriccions

--- src/main/java/spdvi/comparelists/CompareLists.java
+++ src/main/java/spdvi/comparelists/CompareLists.java
@@ -21,47 +21,72 @@ public class CompareLists {
         CompareLists(colouredShapes1, colouredShapes2);
     }
 
-    private static void InitColouredShapes1(ArrayList<ColouredShape> shapes) {
-        shapes.add(new ColouredShape(Shape.CIRCLE, ShapeColor.GREEN));
-        shapes.add(new ColouredShape(Shape.RECTANGLE, ShapeColor.BLACK));
-        shapes.add(new ColouredShape(Shape.TRIANGLE, ShapeColor.BLUE));
-        shapes.add(new ColouredShape(Shape.SQUARE, ShapeColor.WHITE));
-    }
-
-    private static void InitColouredShapes2(ArrayList<ColouredShape> shapes) {
-        shapes.add(new ColouredShape(Shape.CIRCLE, ShapeColor.GREEN));
-        shapes.add(new ColouredShape(Shape.RECTANGLE, ShapeColor.BLACK));
-        shapes.add(new ColouredShape(Shape.TRIANGLE, ShapeColor.BLUE));
-        shapes.add(new ColouredShape(Shape.SQUARE, ShapeColor.WHITE));
-    }
-
-    private static void CompareLists(ArrayList<ColouredShape> shapes1, ArrayList<ColouredShape> shapes2) {
-        if(shapes1.size() != shapes2.size()) {
-            System.out.println("Les llistes son diferents");
-        }
-        
+    private static void CompareIntern(ArrayList<ColouredShape> shapesListWithMoreElements, ArrayList<ColouredShape> shapesListWithLessElements) {
         ArrayList<ColouredShape> diff = new ArrayList<>();
-        for(ColouredShape shapeInList1: shapes1) {
-            for(ColouredShape shapeInList2: shapes2) {
+        for (ColouredShape shapeInList1 : shapesListWithMoreElements) {
+            ColouredShape shapeInList2 = null;
+            boolean shapeIsInList2 = false;
+            for (int i = 0; i < shapesListWithLessElements.size(); i++) {
+                shapeInList2 = shapesListWithLessElements.get(i);
                 if ((shapeInList1.getShape() == shapeInList2.getShape()) && (shapeInList1.getColor() == shapeInList2.getColor())) {
-                    //
+                    // shapeInList1 Exists in List2
+                    shapeIsInList2 = true;
+                    //Aquí ja podriem fer un break;
                 }
-                else {
-                    // Hi ha al menys un element diferent a les dues llistes
-                    diff.add(shapeInList2);
-                }
+            }
+            if (!shapeIsInList2) {
+                // Hi ha al menys un element diferent a les dues llistes
+                diff.add(shapeInList1);
             }
         }
 
         if (diff.size() > 0) {
-            System.out.println("Les llistes son diferents. Els seguents elements de la segona llista no hi son a la primera llista: ");
-            for(ColouredShape cs: diff) {
+            System.out.println("Les llistes son diferents. Els seguents elements de la llista amb més elements no hi son a la llista amb menys elements: ");
+            for (ColouredShape cs : diff) {
                 System.out.println("Shape: " + cs.getShape() + " Colour: " + cs.getColor());
             }
-        } 
-        else {
+        } else {
             System.out.println("Les llistes contenen els mateixos objectes");
         }
-        
+    }
+
+    private static void CompareLists(ArrayList<ColouredShape> shapes1, ArrayList<ColouredShape> shapes2) {
+//        if (shapes1.size() != shapes2.size()) {
+//            System.out.println("Les llistes son diferents");
+//        }
+
+        if (shapes1.size() > shapes2.size()) {
+            // A la primera llista hi ha més elements que a la segona.
+            // És a dir, s'han eliminat elements
+            CompareIntern(shapes1, shapes2);
+        } else if (shapes1.size() < shapes2.size()) {
+            // A la segona llista hi ha més elements que a la primera.
+            // Es a dir, s'ha afegit algún element
+            CompareIntern(shapes2, shapes1);
+        } else {
+            System.out.println("Les llistes contenen els mateixos objectes");
+
+            // Les dues llistes ténen el mateix número d'elements.
+            // Però, són els mateixos?
+//            System.out.println("Les llistes contenen el mateix número d'elements."
+//                    + " Hauriem de comprovar si son els mateixos i,"
+//                    + " cas que no ho fossin, averiguar quins elements diferixen.");
+//            System.out.println("Al context al que esteim, per sincronitzar les dues llistes, "
+//                    + "s'haurien d'afegir els elements que hi han a la llista 2"
+//                    + " a la llista 1, i eliminar de la llista 1 els elements "
+//                    + "de la llista 1 que no hi son a la llista 2.");
+        }
+
+    }
+
+    private static void InitColouredShapes1(ArrayList<ColouredShape> shapes) {
+        shapes.add(new ColouredShape(Shape.CIRCLE, ShapeColor.GREEN));
+        shapes.add(new ColouredShape(Shape.RECTANGLE, ShapeColor.RED));
+    }
+
+    private static void InitColouredShapes2(ArrayList<ColouredShape> shapes) {
+        shapes.add(new ColouredShape(Shape.RECTANGLE, ShapeColor.RED));
+        shapes.add(new ColouredShape(Shape.CIRCLE, ShapeColor.GREEN));
+        shapes.add(new ColouredShape(Shape.TRIANGLE, ShapeColor.BLUE));
     }
 }
